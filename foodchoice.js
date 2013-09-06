2e439c0ed0af7b36d4e8bb34b3886c7b8d949633
Slava Kim <slava@meteor.com>
2013-09-05 21:06:47 -0400

    Write a templates "places" and "restaurant", render data.
    Step 10 is done.

--- foodchoice.js
+++ foodchoice.js
@@ -1,6 +1,9 @@
 Restaurants = new Meteor.Collection('restaurants');
 
 if (Meteor.isClient) {
+  Template.places.topRestaurants = function () {
+    return Restaurants.find({}, { sort: { score: -1 } });
+  };
 }
 
 if (Meteor.isServer) {
