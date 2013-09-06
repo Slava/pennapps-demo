f666a3c6083bef955b084d347012bc2529f50c45
Slava Kim <slava@meteor.com>
2013-09-05 20:57:34 -0400

    Remove everything. Create a collection.

--- foodchoice.js
+++ foodchoice.js
@@ -1,19 +1,7 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to foodchoice.";
-  };
+Restaurants = new Meteor.Collection('restaurants');
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
