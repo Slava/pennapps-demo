72a3cd0425421f7f76375ba1e106832368f705b5
Slava Kim <slava@meteor.com>
2013-09-05 21:08:34 -0400

    Double-click event with updates.
    Step 11

--- foodchoice.js
+++ foodchoice.js
@@ -4,6 +4,13 @@ if (Meteor.isClient) {
   Template.places.topRestaurants = function () {
     return Restaurants.find({}, { sort: { score: -1 } });
   };
+
+  Template.restaurant.events({
+    'dblclick': function () {
+      var rest = this;
+      Restaurants.update(rest._id, { $inc: { score: 5 } });
+    }
+  });
 }
 
 if (Meteor.isServer) {
